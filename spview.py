55f5337ae828ed726b4ddad8fa37cd7163764336
phreme <choqq@jadamspam.pl>
2015-04-07 19:14:07 +0200

    skeleton

---
+++ spview.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Disclaimer: Python 3 used. Python 2 somehow refuses to install properly on my workstation
+
+import requests
+
+
+def get_episode_data():
+    pass
+
+
+def get_url_for_episode(season, episode):
+    return "http://http://southpark.cc.com/full-episodes/s{0}e{1}".format(season, episode)
+
+
+def list_episodes():
+    pass
+
+if __name__ == "main":
+    list_episodes()
